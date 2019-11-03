d48a535453b116cc43c1b6bdf8f21b0554a3650b
alissonferreirasv <alissonferreirasv@gmail.com>
2019-11-02 23:44:00 -0300

    padrao

--- preprocess.py
+++ preprocess.py
@@ -42,7 +42,7 @@ def write_metadata(metadata, out_dir):
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--base_dir', default=os.path.expanduser('~/root/tacotron'))
+  parser.add_argument('--base_dir', default=os.path.expanduser('~/tacotron'))
   parser.add_argument('--output', default='training')
   parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech', 'ptbr'])
   parser.add_argument('--num_workers', type=int, default=cpu_count())
